6073913084715bb8ff5db3bebc8f603d1cfd3902
J-Sparks <68030345+J-Sparks@users.noreply.github.com>
2020-09-27 12:54:28 -0400

    Update Feature selection using R.R

--- Feature selection using R.R
+++ Feature selection using R.R
@@ -11,20 +11,27 @@ library(randomForest)
 library(caret)
 
 #data
-CSEdata <- read.csv("~/Data_IR/CSEdata.csv", stringsAsFactors=TRUE); View(CSEdata)
+CSEdata <- read.csv("~/Data_IR/updated1CSEdata.csv", stringsAsFactors=TRUE); View(CSEdata)
 #CSEdata$GPA_ENTERING_SECOND_FALL
 str(CSEdata)
 apply(CSEdata, 2,p) # find NA
 
-
+#find average 1st fall GPA
+AVEGPA <- CSEdata$FIRST_FALL_GPA
+AVEGPA[(is.na(AVEGPA))] <- 0
+CSEdata$FIRST_FALL_GPA <- AVEGPA
+mean(CSEdata$FIRST_FALL_GPA)
 
 # data set
 CSEdataAveGPA <- CSEdata %>% 
-  mutate(aveGPA = ifelse(GPA_ENTERING_SECOND_FALL>= 2.77,"above2.77","below2.77")) %>% 
-  filter(ENTRY_COLLEGE=="HMCSE") %>% 
-  select(#ENTRY_PROGRAM,ENTRY_DEPARTMENT,
-    GENDER,ETHNICITY,NATION,aveGPA,APPLICANT_TIER,
-    AGE_AT_ENTRY,
+  
+  mutate(aveGPA = ifelse(FIRST_FALL_GPA >= 2.77,"above2.77","below2.77")) %>% 
+  filter(ETHNICITY=="African American") %>% 
+  select(#ENTRY_PROGRAM,
+    ENTRY_DEPARTMENT,
+    #ETHNICITY,
+    aveGPA,APPLICANT_TIER,
+    #AGE_AT_ENTRY,
     #COUNTY,STATE,
          
          #GPA_HIGHSCHOOL,
@@ -34,9 +41,8 @@ CSEdataAveGPA <- CSEdata %>%
 
 CSEdataAveGPA$aveGPA <- as.factor(CSEdataAveGPA$aveGPA) 
 CSEdataAveGPA$APPLICANT_TIER <- as.factor(CSEdataAveGPA$APPLICANT_TIER)
-str(CSEdataAveGPA)
-
-apply(CSEdataAveGPA, 2, p)
+glimpse(CSEdataAveGPA)
+sapply(CSEdataAveGPA,function(x){sum(is.na(x)/length(x)*100)} )
 
 # check multicollinearity
 cordata <- CSEdataAveGPA %>% 
@@ -45,13 +51,16 @@ glimpse(cordata)
 library(PerformanceAnalytics)
 chart.Correlation(cordata)
 
-#Feature selection
-set.seed(111) #for random saple to generate
+
 
 #if youhave NAs use rfImpute
 data.imputed <- rfImpute(aveGPA~., data=CSEdataAveGPA, iter=6)
 
-borutaCSE <- Boruta(aveGPA~., data = CSEdataAveGPA, doTrace=2, maxRuns=50 ) #dotrace =2 #maxruns = 50 more chance of importance
+
+#using Boruts
+#Feature selection
+set.seed(111) #for random saple to generate
+borutaCSE <- Boruta(aveGPA~., data = CSEdataAveGPA, doTrace=2, maxRuns=30 ) #dotrace =2 #maxruns = 100 more chance of importance
 print(borutaCSE)
 
 
@@ -85,14 +94,11 @@ RFMCSE11 <- randomForest(aveGPA ~ GENDER + ETHNICITY + AGE_AT_ENTRY + NATION + F
                               HOURS_BROUGHT_TO_UNIVERSITY, data = aveGPATrainingSet, proximity=TRUE) # get nonrejectedfacotrs
 RFMCSE11
 
-# using performanceanalysis
-model <- train(aveGPA~., data=CSEdataAveGPA, method="rf", TuneLength=3,trControl=trainControl(method = "cv",number=10,classProbs = TRUE))
-pred <- predict(model, CSEdataAveGPA, "prob")
-head(pred,10)
 
-RFMCSE7 <- randomForest(aveGPA ~ GENDER + FIRST_FALL_BRIGHT_FUTURES_AMOUNT + FIRST_FALL_NEED_BASED_LOANS_AMOUNT + 
-                          FIRST_FALL_NON_NEED_BASED_LOANS_AMOUNT + AP_CREDITS + APPLICANT_TIER + 
-                          HOURS_BROUGHT_TO_UNIVERSITY, data=aveGPATrainingSet)
+
+RFMCSE7 <- randomForest(aveGPA ~ APPLICANT_TIER + FIRST_FALL_PELL_AMOUNT + FIRST_FALL_BRIGHT_FUTURES_AMOUNT + 
+                          FIRST_FALL_NEED_BASED_LOANS_AMOUNT + FIRST_FALL_NON_NEED_BASED_LOANS_AMOUNT + 
+                          AP_CREDITS + HOURS_BROUGHT_TO_UNIVERSITY, data=aveGPATrainingSet)
 RFMCSE7
 #prediction and confusion matrix
 p <- predict(RFMCSE, aveGPATrainingSet)
@@ -102,6 +108,7 @@ confusionMatrix(pt, aveGPATestingSet$aveGPA)#0.6974
 
 
 p11 <- predict(RFMCSE11, aveGPATrainingSet)
+head(p11,10)
 confusionMatrix(p11, aveGPATrainingSet$aveGPA)#0.9357
 importance(RFMCSE11)
 varImpPlot(RFMCSE11)
@@ -112,6 +119,43 @@ p7 <- predict(RFMCSE7, aveGPATrainingSet)
 confusionMatrix(p7, aveGPATrainingSet$aveGPA)#0.8107 
 p7t <- predict(RFMCSE7, aveGPATestingSet)
 confusionMatrix(p7t, aveGPATestingSet$aveGPA)#0.7044
+
+
+###  overfitting issue
+# using LOOCV RESAMPLING
+model.loocv <- train(aveGPA~., data=aveGPATrainingSet, method="rf", TuneLength=3,trControl=trainControl(method = "LOOCV",
+                                                                                              number=10,classProbs = TRUE))
+# using K-folder corss validation resampling
+model.cv <- train(aveGPA~., data=aveGPATrainingSet, method="rf", TuneLength=3,trControl=trainControl(method = "cv",
+                                                                                              number=10,classProbs = TRUE))
+# using bootstrapping resampling
+model.boot <- train(aveGPA~., data=aveGPATrainingSet, method="rf", TuneLength=3,trControl=trainControl(method = "boot",
+                                                                                                     number=10,classProbs = TRUE))
+# using repeated k-fold cross validation resampling
+model.repeatedcv <- train(aveGPA~., data=aveGPATrainingSet, method="rf", TuneLength=3,trControl=trainControl(method = "repeatedcv",
+                                                                                                       number=10,classProbs = TRUE))
+# prediction
+ploocv <- predict(model.loocv, aveGPATrainingSet, "prob")
+pcv <- predict(model.cv, aveGPATrainingSet, "prob")
+pboot <- predict(model.boot, aveGPATrainingSet, "prob")
+prepeatedcv <- predict(model.repeatedcv, aveGPATrainingSet, "prob")
+
+confusionMatrix(ploocv, aveGPATrainingSet$aveGPA)#0.8107
+confusionMatrix(pcv, aveGPATrainingSet$aveGPA)#0.8107
+confusionMatrix(pboot, aveGPATrainingSet$aveGPA)#0.8107
+confusionMatrix(prepeatedcv, aveGPATrainingSet$aveGPA)#0.8107
+
+# Models
+models <- c("LOOCV","CV","BOOT","REPEATEDCV")
+accuracy <- c(round(model.loocv$overall["Accuracy"], 3),
+              round(model.cv$overall["Accuracy"], 3),
+              round(model.boot$overall["Accuracy"], 3),
+              round(model.repeatedcv$overall["Accuracy"], 3))
+
+accy <- data.frame(models, accuracy)
+
+
+
 
 ######################
 ## Multicollinearity###
@@ -177,4 +221,5 @@ ggplot(data=mds.data, aes(x=X, y=Y, label=Sample)) +
   ylab(paste("MDS2 - ", mds.var.per[2], "%", sep="")) +
   ggtitle("MDS plot using (1 - Random Forest Proximities)")
 # ggsave(file="random_forest_mds_plot.pdf")
+
 
